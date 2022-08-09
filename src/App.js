fa63ef9f1e9450dcde144da637ad22de7c42fb3c
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-08-09 06:06:43 +0300

    Created an async fun. to fetch data from API & used useState hook to update state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
 import Dice from './assets/icon-dice.svg';
 import PatterDivider from './assets/pattern-divider-desktop.svg'
 
 function App() {
+
+  const [advice, setAdvice] = useState({
+    id: '',
+    advice: '',
+  });
+
+  const fetchAdvice = async() => {
+    const url = "https://api.adviceslip.com/advice";
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+        setAdvice({
+          id: json.slip.id,
+          advice: json.slip.advice
+        });
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="h-screen w-screen bg-gray-800 flex justify-center relative">
       <div className="bg-white sm:w-1/5 w-96 h-80 absolute top-1/4 rounded-lg bg-slate-700">
         <div className="w-full h-1/5 pt-10 flex justify-center">
-          <p className="text-green-300 text-xs tracking-widest">ADVICE #117</p>
+          <p className="text-green-300 text-xs tracking-widest">ADVICE {advice.id}</p>
         </div>
-        <div className="w-3/4 sm:h-36 h-40 mt-4 m-auto">
+        <div className="w-3/4 sm:h-36 h-40 mt-6 m-auto">
           <p className="text-white sm:text-md text-xl text-center font-bold tracking-wide">
-            "it is easy to sit up and take notice,
-            what's difficult is
-            getting up and taking action."
+            {advice.advice}
           </p>
         </div>
-        <div className="w-full flex justify-center pt-2">
+        <div className="w-full flex justify-center">
           <img className="w-3/4"
                src={PatterDivider}
                alt="pattern"
@@ -26,7 +45,9 @@ function App() {
           <div className="bg-green-300 shadow hover:shadow-lg hover:shadow-emerald-300 h-12 w-12 rounded-full justify-center">
             <img className="m-auto mt-3"
                  src={Dice}
-                 alt="dice"/>
+                 alt="dice"
+                 onClick={fetchAdvice}
+            />
           </div>
         </div>
       </div>
